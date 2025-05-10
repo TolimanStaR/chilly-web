59bcb295c0d8346c222cd0eab1086018a637422a
Matvey Baryshev <matvey33.baryshev@mail.ru>
2025-05-10 12:32:07 +0300

    feat: new main page with landing

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,21 +1,40 @@
-import { Link } from "react-router-dom";
-import {Icon} from "@/components/icons/Icon";
+import { Link, useLocation } from "react-router-dom";
+import { Icon } from "@/components/icons/Icon";
 
 export const Header = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
-    <header className="bg-base-0 text-white h-[48px] px-4 shadow fixed top-0 left-0 right-0 z-50">
+    <header
+      className={`${isHomePage ? "bg-transparent shadow-none" : "bg-base-0 shadow"} text-white h-[48px] px-4 fixed top-0 left-0 right-0 z-50 transition-all duration-300`}
+    >
       <div className="container mx-auto max-w-[1000px] flex h-full justify-between items-center">
         <Link to="/" className="flex flex-row gap-2 items-center">
-          <Icon name={"chilly"} size={42}/>
+          <Icon name={"chilly"} size={42} />
           <p className={"text-xl font-bold text-red-50"}>Chilly</p>
-          <p className={"text-xl text-base-100"}>{" для бизнеса"}</p>
+          <p className={`text-xl ${isHomePage ? "text-white" : "text-base-100"}`}>
+            {" для бизнеса"}
+          </p>
         </Link>
 
         <nav>
           <ul className="flex space-x-6">
             <li className={"flex items-center"}>
-              <Link to="/profile" className="text-lg text-base-100">
+              <Link
+                to="/profile"
+                className={`text-lg ${isHomePage ? "text-white" : "text-base-100"}`}
+              >
                 Профиль
+              </Link>
+            </li>
+
+            <li className={"flex items-center"}>
+              <Link
+                to="/faq"
+                className={`text-lg ${isHomePage ? "text-white" : "text-base-100"}`}
+              >
+                FAQ
               </Link>
             </li>
           </ul>
@@ -23,4 +42,4 @@ export const Header = () => {
       </div>
     </header>
   );
-}
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import {IconButton, Radio, TextInput} from "@/components/input";
-import {Checkbox} from "@/components/input";
+import {Button} from "@/components/input";
 import {useEffect} from "react";
+import {Link} from "react-router-dom";
 
 export const Home = () => {
   useEffect(() => {
@@ -8,18 +8,38 @@ export const Home = () => {
   }, []);
 
   return (
-    <div className={"flex flex-col w-full max-w-[1000px] mx-auto items-center justify-center gap-2"}>
-      <IconButton icon={"link"} size={"S"}/>
-      <IconButton icon={"link"} />
-      <IconButton icon={"link"} palette={"gray"} size={"S"}/>
-      <IconButton icon={"link"} palette={"gray"}/>
+    <section className={"bg-white h-full py-4 px-4 text-center"}>
+      <img
+        src={`main_background.png`}
+        alt={"Человек с телефоном"}
+        className={"absolute inset-0 w-full h-full object-cover"}
+      />
 
-      <Checkbox text={"Nteasdsa"}/>
-      <Radio text={"Nteasdsa"}/>
+      <div className={"absolute inset-0 bg-base-100/50 h-full w-full px-4 mx-auto flex flex-col items-center justify-center gap-8"}>
+        <div className={"flex flex-col bg-base-0/60 p-8 rounded-2xl backdrop-blur-md gap-8"}>
+          <h1 className={"text-h4 font-semibold"}>
+            Тысячи людей выбирают <span className={"text-red-60"}>Chilly</span>, чтобы отдохнуть.
+          </h1>
 
-      <TextInput title={"sd"}/>
-      <TextInput title={"sd"} inputSize={"S"}/>
-      <TextInput title={"sd"}/>
-    </div>
+          <p className={"text-base-100 text-H5 max-w-xl"}>
+            Присоединяйтесь, добавляйте свои места и привлекайте новых клиентов в бизнес.
+          </p>
+
+          <div className={"flex gap-4 flex-col justify-center items-center"}>
+            <Link to={"/register"}>
+              <Button size={"M"}>
+                Начать пользоваться Chilly
+              </Button>
+            </Link>
+
+            <Link to={"/faq"}>
+              <Button variant={"secondary"} size={"S"} palette={"gray"}>
+                Перейти к FAQ
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
